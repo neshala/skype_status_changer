90fd2d8150db7d7c555be31681a760fa1e4d2361
nenadmilosavljevic <lazarevac@gmail.com>
2016-01-13 21:45:55 +0100

    Initial Commit

---
+++ status_changer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# filename: Skype Changer
+# usage from Terminal prompt: sh status_changer.sh "Online"
+
+status=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+if [ $status != 'online' ] && [ $status != 'away' ] && [ $status != 'dnd' ] && [ $status != 'invisible' ]  && [ $status != 'offline' ]; then
+    echo 'Invalid Skype status !!!'
+    exit 1
+fi
+
+osascript -e '
+tell application "System Events" to (name of processes) contains "Skype"
+if the result is true then
+	tell application "Skype"
+		send command "SET USERSTATUS '$status'" script name "Skype Changer"
+		display notification "Status changed to '$status'" with title "Skype Changer"
+		say "Skype Status changed to '$status' "
+	end tell
+end if
+'
