a4e539158b54efce16afb45c443612da6ea9d2db
Nenad Milosavljevic <lazarevac@gmail.com>
2016-01-29 15:27:25 +0100

    Check should Skype status be changed. If request status is the same as current status, script will be aborted.

--- status_changer.sh
+++ status_changer.sh
@@ -2,16 +2,26 @@
 # filename: Skype Changer
 # usage from Terminal prompt: sh status_changer.sh "Online"
 
+# check is argument passed
 if [ "$#" -ne 1 ]
 then
   echo "Please provide status as argument."
   exit 1
 fi
 
+# check is status valid
 status=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
-if [ $status != 'online' ] && [ $status != 'away' ] && [ $status != 'dnd' ] && [ $status != 'invisible' ]  && [ $status != 'offline' ]; then
+if [ "$status" != 'online' ] && [ "$status" != 'away' ] && [ "$status" != 'dnd' ] && [ "$status" != 'invisible' ]  && [ "$status" != 'offline' ]; then
     echo 'Invalid Skype status !!!'
+    exit 1
+fi
+
+# get current status, and check is change needed
+online_status=`osascript -e "tell application \"Skype\" to send command \"GET USERSTATUS\" script name \"my script\"" | tr '[:upper:]' '[:lower:]'`
+
+if [ "$online_status" = "userstatus $status" ]; then
+    echo "Nothing to do, skype status is already $status ."
     exit 1
 fi
 
